30abb8a4dec98bbb9f103e65e593cd4f7858b731
dajeong09 <chic519@gmail.com>
2022-11-26 21:24:08 +0900

    first commit

---
+++ src/main/java/com/example/cuckoolandback/CuckooLandBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuckoolandback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuckooLandBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuckooLandBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cuckoolandback/CuckooLandBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuckoolandback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuckooLandBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
