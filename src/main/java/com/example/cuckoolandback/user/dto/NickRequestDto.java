0f803dc3422871aec3868b4134197150cd3a38d9
Meanz12 <meanz1331@gmail.com>
2022-12-06 14:58:55 +0900

    Modify : nickname validation modification

--- src/main/java/com/example/cuckoolandback/user/dto/NickRequestDto.java
+++ src/main/java/com/example/cuckoolandback/user/dto/NickRequestDto.java
@@ -9,8 +9,8 @@ import javax.validation.constraints.Pattern;
 @ApiModel(value = "닉네임 객체", description = "닉네임 정보를 받기위한 객체")
 @Getter
 public class NickRequestDto {
-    @ApiModelProperty(value="닉네임", example = "뻐꾹", required = true)
+    @ApiModelProperty(value="닉네임", example = "뻐꾹이1", required = true)
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z-0-9]{2,8}$", message = "닉네임은 특수문자를 제외한 2~8자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z-0-9]{4,16}$", message = "닉네임은 특수문자를 제외한 4~16자리여야 합니다.")
     private String nickname;
 }
--- src/main/java/com/example/cuckoolandback/user/dto/RegisterRequestDto.java
+++ src/main/java/com/example/cuckoolandback/user/dto/RegisterRequestDto.java
@@ -15,9 +15,9 @@ public class RegisterRequestDto {
     @Pattern(regexp = "^[a-zA-Z-0-9]{4,16}$", message = "아이디는 4~16자리 영문,숫자를 사용합니다.")
     private String memberId;
 
-    @ApiModelProperty(value="닉네임", example = "뻐꾹이", required = true)
+    @ApiModelProperty(value="닉네임", example = "뻐꾹이1", required = true)
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z-0-9]{2,8}$", message = "닉네임은 특수문자를 제외한 2~8자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z-0-9]{4,16}$", message = "닉네임은 특수문자를 제외한 4~16자리여야 합니다.")
     private String nickname;
 
     @ApiModelProperty(value="비밀번호", example = "Abcd123!", required = true)
