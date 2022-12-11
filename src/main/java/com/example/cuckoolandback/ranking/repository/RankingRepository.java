c72db2ddd148636fe8e42be3d9426bddd447bd27
Hiluxy <sunset.lhj@gmail.com>
2022-12-11 16:17:22 +0900

    fix : 랭킹 repository 삭제

--- src/main/java/com/example/cuckoolandback/ranking/repository/RankingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.cuckoolandback.ranking.repository;
-
-import com.example.cuckoolandback.friend.domain.Friend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankingRepository extends JpaRepository<Friend, Long> {
-
-}
