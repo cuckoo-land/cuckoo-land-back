37aa047773d008c5267ca21d03f6791363183158
hm5938 <hmlee5938@gmail.com>
2022-12-16 04:09:29 +0900

    feat : Majority, Picture, Vs 객체 추가

---
+++ src/main/java/com/example/cuckoolandback/majority/domain/Vs.java
@@ -0,0 +1,32 @@
+package com.example.cuckoolandback.majority.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Vs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    Long roomId;
+
+    @Column(nullable = false)
+    int roundNum;
+
+    @Column(nullable = false)
+    Long itemId1;
+
+    @Column(nullable = false)
+    Long itemId2;
+
+    @Column
+    Long winner;
+}
