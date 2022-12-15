ba17369709f42f7a546a86cfb1d8a9df0669e0b6
hm5938 <hmlee5938@gmail.com>
2022-12-16 04:12:49 +0900

    feat : 다수결게임 service 생성

---
+++ src/main/java/com/example/cuckoolandback/majority/service/MajorityService.java
@@ -0,0 +1,40 @@
+package com.example.cuckoolandback.majority.service;
+
+import com.example.cuckoolandback.common.exception.CustomException;
+import com.example.cuckoolandback.common.exception.ErrorCode;
+import com.example.cuckoolandback.majority.domain.Majority;
+import com.example.cuckoolandback.majority.domain.Picture;
+import com.example.cuckoolandback.majority.domain.Vs;
+import com.example.cuckoolandback.majority.dto.*;
+import com.example.cuckoolandback.majority.repository.MajorityRepository;
+import com.example.cuckoolandback.majority.repository.PictureRepository;
+import com.example.cuckoolandback.majority.repository.VsRepository;
+import com.example.cuckoolandback.room.domain.Participant;
+import com.example.cuckoolandback.room.domain.Room;
+import com.example.cuckoolandback.room.domain.RoomStatus;
+import com.example.cuckoolandback.room.dto.MessageResponseDto;
+import com.example.cuckoolandback.room.repository.ParticipantRepository;
+import com.example.cuckoolandback.room.repository.RoomRepository;
+import com.example.cuckoolandback.room.service.RoomService;
+import com.example.cuckoolandback.user.domain.Member;
+import com.example.cuckoolandback.user.domain.UserDetailsImpl;
+import com.example.cuckoolandback.user.dto.MemberResponseDto;
+import com.example.cuckoolandback.user.repository.MemberRepository;
+import com.example.cuckoolandback.websocket.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MajorityService {
+    private final SimpMessageSendingOperations sendingOperations;
+}
